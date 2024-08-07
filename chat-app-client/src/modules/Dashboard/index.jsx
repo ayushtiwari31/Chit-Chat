b888ce33b32265353588db78fa493e6d13a46854
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2024-08-07 16:15:38 +0530

    changes

--- chat-app-client/src/modules/Dashboard/index.jsx
+++ chat-app-client/src/modules/Dashboard/index.jsx
@@ -27,7 +27,7 @@ const Dashboard = () => {
   const [userConversations, setUserConversations] = useState({});
 
 	useEffect(() => {
-		const socketInstance = io('https://chit-chat-5-wnwg.onrender.com:3000');
+		const socketInstance = io('https://chit-chat-5-wnwg.onrender.com:443');
         setSocket(socketInstance);
 
         return () => {
